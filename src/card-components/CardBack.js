e6d724ff24b16e8e4b703753dbcb2de02567d2f6
Damini varu <daminivaru@Daminis-MacBook-Pro.local>
2019-11-21 10:16:58 -0600

    Done.

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -9,20 +9,34 @@ import five from '../assets/stars/5-stars.png'
 const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 
 export default class CardBack extends Component {
+  //For IMDBRating, you will need to finish writing the method generateRatingElement(), which should do the following:
+// 
 
   generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
+    if(this.props.IMDBRating === null){ //if the IMDBRating prop is null, 
+      return (<h4>No Rating Found</h4>) //return an <h4> with the contents 'No Rating Found'
+    } else { //otherwise, 
+      return ( // return
+        <img src = {imgMapper[this.props.IMDBRating]} alt=""/> // <img src={imgMapper[prop]} alt="" /> (using the correct prop)
+      )
+    }
   }
 
+// CardBack will display the title, genres and IMDB rating.
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+          {/* movie title */}
+        <h3 className="title">{this.props.title}</h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        {/* rating element for movie */}
+        {this.generateRatingElement
+        ()}
         <span />
-        <h5 className="genres"></h5>
+        {/*For genres, join each genre together into string with commas separating each.*/}
+        <h5 className="genres">{this.props.genres.join(',')}</h5>
       </div>
     )
   }
 }
+
